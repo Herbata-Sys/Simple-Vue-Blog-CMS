18ab4923bff69669bb3f578232057ffbb32f478d
Herbata-Sys <56880880+Herbata-Sys@users.noreply.github.com>
2019-10-23 22:01:48 +0200

    Remove my config file from /api

--- public/api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  //database config
-  $servername = 'localhost'; //host
-  $username = 'root'; //username
-  $password = ''; //password
-  $database = 'testowa'; //database
-  $recaptcha_server_key = '6LeZArQUAAAAAN-NyxmNc5GXmCy5308z2DI3lsTk'; //recaptcha serverside key
-?>
